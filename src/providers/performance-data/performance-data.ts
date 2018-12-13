393c3724baecc354a52fc04cb4d2f8ebdaf7cb50
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-13 16:15:57 +0100

    Imports and injects Angular2TokenService to PerformanceDataProvider

--- src/providers/performance-data/performance-data.ts
+++ src/providers/performance-data/performance-data.ts
@@ -1,17 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the PerformanceDataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class PerformanceDataProvider {
+  constructor(private _tokenService: Angular2TokenService) {}
 
-  constructor(public http: HttpClient) {
-    console.log('Hello PerformanceDataProvider Provider');
+  saveData(data) {
+    return this._tokenService.post('performance_data', data).map(data => data);
   }
-
 }
